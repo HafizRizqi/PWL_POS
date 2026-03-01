73851c0fde80d6fea78ea2c7f7a425fbb5bea4cb
HAFIZ RIZQI HERNANDA <hafizhernanda5@gmail.com>
2026-03-01 07:30:18 +0700

    Jawaban soal Praktikum pada Jobsheet 2 - PWL

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+
+use App\Http\Controllers\ProductController;
+
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
 });
+
+Route::get('/user/{id}/{name}', [UserController::class, 'profile']);
+
+Route::get('/sales', [SalesController::class, 'sales']);
