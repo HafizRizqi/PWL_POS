2504acff9791b68adad683e6604437a4ebc01602
HAFIZ RIZQI HERNANDA <hafizhernanda5@gmail.com>
2026-03-01 22:49:17 +0700

    Laporan Praktikum 4 Pada Jobsheet-03

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LevelController;
 
-Route::get('/', [HomeController::class, 'index']);
-
-use App\Http\Controllers\ProductController;
-
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/user/{id}/{name}', [UserController::class, 'profile']);
+Route::get('/level', [LevelController::class, 'index']);
 
-Route::get('/sales', [SalesController::class, 'sales']);
+
