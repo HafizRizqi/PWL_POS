8758352e622ade6ada365e05cb92b04dfaef5f3d
HAFIZ RIZQI HERNANDA <hafizhernanda5@gmail.com>
2026-03-01 23:01:01 +0700

    Laporan Praktikum 5 pada Jobsheet-03

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,13 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
 
