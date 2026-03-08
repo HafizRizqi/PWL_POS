f89610792ab16ad4569472f01fe74f7bbbb7479f
HAFIZ RIZQI HERNANDA <hafizhernanda5@gmail.com>
2026-03-08 10:37:56 +0700

    Jobsheet-04 Praktikum-2.5  Attribute Changes Langkah 1 - 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,32 @@ class UserController extends Controller
 {
      public function index()
     {
-       $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
-        $user->save(); // Menyimpan data user ke database
+       $user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ]);
 
-        return view ('user',['data' => $user]); // Mengirim data user ke view 'user'
+        $user->username = 'manager56';
+
+        // Mengecek status perubahan atribut
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // false
+        $user->isDirty(['nama', 'username']); // true
+
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // true
+        $user->isClean(['nama', 'username']); // false
+
+        $user->save();
+
+        $user->isDirty(); // false
+        $user->isClean(); // true
+        dd($user->isDirty()); // false setelah disimpan
+
+        //return view ('user',['data' => $user]); // Mengirim data user ke view 'user'
     }
 }
