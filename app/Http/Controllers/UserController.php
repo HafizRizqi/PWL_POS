e0dc074d574f473dde79a803679bf8b2ffba263a
HAFIZ RIZQI HERNANDA <hafizhernanda5@gmail.com>
2026-03-08 10:03:48 +0700

    Jobsheet-04 Praktikum-2.2 Not Found Exception Langkah  3 - 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,8 @@ class UserController extends Controller
 {
      public function index()
     {
-         $user = UserModel::findOrFail(1); // Mengambil data user dengan ID 1, jika tidak ditemukan akan menghasilkan error 404
+         $user = UserModel::where('username', 'manager9')->firstOrFail();
+
         return view ('user',['data' => $user]); // Mengirim data user ke view 'user'
     }
 }
