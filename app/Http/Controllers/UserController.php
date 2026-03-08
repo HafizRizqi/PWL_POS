8cafe0b2797f39af6d4016874141c85b8895c6af
HAFIZ RIZQI HERNANDA <hafizhernanda5@gmail.com>
2026-03-08 09:56:14 +0700

    Jobsheet-04 Praktikum-2.1 Retrieving Single Models Langkah 8 - 9

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,10 @@ class UserController extends Controller
 {
      public function index()
     {
-        $user = UserModel::firstWhere('level_id',1); // Mengambil data user dengan level_id 1 dari tabel m_user
+        $user = UserModel::findOr(1, ['username', 'nama'], function () {
+            abort(404);
+        });
+
         return view ('user',['data' => $user]); // Mengirim data user ke view 'user'
     }
 }
